f721fa6ce330686568539d020df0582f9f4c5593
Felipe Gabba <felipegabba24@gmail.com>
2023-08-07 22:06:08 -0300

    final entrega3

--- paginas/main.js
+++ paginas/main.js
@@ -4,9 +4,8 @@ const cerrar = document.querySelector("#cerrar");
 
 abrir.addEventListener("click", () => {
     nav.classList.add("visible");
-});
+})
 
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
-
-});
+})
